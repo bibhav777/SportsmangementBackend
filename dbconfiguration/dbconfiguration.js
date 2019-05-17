612bfc2308aab612cf127c6a106440946568a991
bibhav777 <bibhav.sapkotaqa@gmail.com>
2019-05-17 23:22:53 +0545

    database created and connected

--- dbconfiguration/dbconfiguration.js
+++ dbconfiguration/dbconfiguration.js
@@ -1,5 +1,5 @@
 var Sequelize= require('sequelize');
-var sequelize= new Sequelize('practicedb','root','Degea1234!',{
+var sequelize= new Sequelize('spmanagement','root','Degea1234!',{
  host: 'localhost',
  dialect: 'mysql',
  logging: false
@@ -16,3 +16,8 @@ console.log('db connected sucessfully');
 .catch(function(err){
 	console.log(err);
 });
+
+
+module.exports{
+	Sequelize, sequelize
+}
